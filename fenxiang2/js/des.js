e9f42cee0d5720b4e1b546cbfc1a329d120c7696
chengyue77 <1452532199@qq.com>
2018-06-01 14:59:34 +0800

    xcc

---
+++ fenxiang2/js/des.js
@@ -0,0 +1,8 @@
+$(".kd").on('tap',function(){
+	$(".vos").css("display","block");
+	$(".soc").css("display","block");
+})
+$(".hua").on('tap',function(){
+	$(".vos").css("display","none");
+	$(".soc").css("display","none");
+})
